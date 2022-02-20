21dadf2474110a135bbec96da9ab44cb7a63dab0
Louis Villain <louis.villainl@gmail.com>
2022-02-20 14:14:04 +0100

    new homepage

--- front/src/components/Loading/index.js
+++ front/src/components/Loading/index.js
@@ -4,25 +4,25 @@ import './style.scss';
 
 const Loading = () => (
   <>
-  <div className='body'>
-    <span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-    </span>
-    <div className='base'>
-      <span></span>
-      <div className='face'></div>
+    <div className='body'>
+      <span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </span>
+      <div className='base'>
+        <span></span>
+        <div className='face'></div>
+      </div>
     </div>
-  </div>
-  {/* <div className='longfazers'>
-    <span></span>
-    <span></span>
-    <span></span>
-    <span></span>
-  </div> */}
-  <h1 className="title">Chargement...</h1>
+    {/* <div className='longfazers'>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div> */}
+    <h1 className="title">Chargement...</h1>
   </>
 );
 
